45dff18687e409d8bab536c6ea51cff3f9bbe79e
Ariful <arifratul41@gmail.com>
2021-04-28 13:14:54 +0600

    adds json server save

--- pages/calendar.js
+++ pages/calendar.js
@@ -1,23 +1,33 @@
 import React, { useState } from 'react';
-
 import { DatePicker } from "antd";
 import "antd/dist/antd.css";
 
-export default function Home() {
-    const [date, onChange] = useState(new Date());
-    const getCalendarData = () => {
-        // individual date month year extraction format dd-mm-yyyy
-        let ye = new Intl.DateTimeFormat('en', { year: 'numeric' }).format(date);
-        let mo = new Intl.DateTimeFormat('en', { month: '2-digit' }).format(date);
-        let da = new Intl.DateTimeFormat('en', { day: '2-digit' }).format(date);
-        console.log(`${da}-${mo}-${ye}`);
+function formatDate(dateRaw) {
+    let ye = new Intl.DateTimeFormat('en', { year: 'numeric' }).format(dateRaw);
+    let mo = new Intl.DateTimeFormat('en', { month: '2-digit' }).format(dateRaw);
+    let da = new Intl.DateTimeFormat('en', { day: '2-digit' }).format(dateRaw);
+    return `${da}-${mo}-${ye}`;
 
-        // en-GB locale format dd/mm/yyyy
+    /*
+    * // en-GB locale format dd/mm/yyyy
         let o = new Intl.DateTimeFormat("en-GB" , {
             dateStyle: "short"
         });
-        console.log(o.format(date))
+    * */
+}
 
+export default function Home() {
+    const [date, onChange] = useState(new Date());
+    const callJsonServer = () => {
+        fetch('http://localhost:3001/posts', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({date: formatDate(date)})
+        })
+            .then(response => response.json())
+            .then(json => console.log(json))
     }
     return (
         <div>
@@ -27,7 +37,7 @@ export default function Home() {
                     placeholder={"DD-MM-YYYY"}
                     format={"DD-MM-YYYY"}
                 />
-                <button onClick={getCalendarData}>React Calendar</button>
+                <button onClick={callJsonServer}>React Calendar</button>
             </div>
 
         </div>
