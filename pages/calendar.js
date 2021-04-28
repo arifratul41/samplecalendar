fe5f213365c864cc842558a78e7b9511e4b6d403
Ariful <arifratul41@gmail.com>
2021-04-28 11:45:57 +0600

    antd datepicker

--- pages/calendar.js
+++ pages/calendar.js
@@ -1,36 +1,35 @@
-import Calendar from 'react-calendar'
 import React, { useState } from 'react';
-import DatePicker from "react-datepicker";
 
-import 'react-calendar/dist/Calendar.css';
-import "react-datepicker/dist/react-datepicker.css";
+import { DatePicker } from "antd";
+import "antd/dist/antd.css";
 
 export default function Home() {
-    const [startDate, setStartDate] = useState(new Date());
+    const [date, onChange] = useState(new Date());
     const getCalendarData = () => {
         // individual date month year extraction format dd-mm-yyyy
-        let ye = new Intl.DateTimeFormat('en', { year: 'numeric' }).format(startDate);
-        let mo = new Intl.DateTimeFormat('en', { month: '2-digit' }).format(startDate);
-        let da = new Intl.DateTimeFormat('en', { day: '2-digit' }).format(startDate);
+        let ye = new Intl.DateTimeFormat('en', { year: 'numeric' }).format(date);
+        let mo = new Intl.DateTimeFormat('en', { month: '2-digit' }).format(date);
+        let da = new Intl.DateTimeFormat('en', { day: '2-digit' }).format(date);
         console.log(`${da}-${mo}-${ye}`);
 
         // en-GB locale format dd/mm/yyyy
         let o = new Intl.DateTimeFormat("en-GB" , {
             dateStyle: "short"
         });
-        console.log(o.format(startDate))
+        console.log(o.format(date))
 
     }
     return (
         <div>
             <div>
                 <DatePicker
-                    dateFormat="dd-MM-yyyy"
-                    placeholderText={"DD-MM-YYYY"}
-                    selected={startDate}
-                    onChange={date => setStartDate(date)} />
+                    onChange={onChange}
+                    placeholder={"DD-MM-YYYY"}
+                    format={"DD-MM-YYYY"}
+                />
                 <button onClick={getCalendarData}>React Calendar</button>
             </div>
+
         </div>
     );
 }
