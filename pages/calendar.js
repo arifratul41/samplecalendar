df22b4c23caa6a239eba697b6bfb7a84510e4f78
Ariful <arifratul41@gmail.com>
2021-04-28 11:12:11 +0600

    implements datepicker

--- pages/calendar.js
+++ pages/calendar.js
@@ -1,30 +1,35 @@
 import Calendar from 'react-calendar'
 import React, { useState } from 'react';
+import DatePicker from "react-datepicker";
+
 import 'react-calendar/dist/Calendar.css';
+import "react-datepicker/dist/react-datepicker.css";
 
 export default function Home() {
-    const [value, onChange] = useState(new Date());
+    const [startDate, setStartDate] = useState(new Date());
     const getCalendarData = () => {
         // individual date month year extraction format dd-mm-yyyy
-        let ye = new Intl.DateTimeFormat('en', { year: 'numeric' }).format(value);
-        let mo = new Intl.DateTimeFormat('en', { month: '2-digit' }).format(value);
-        let da = new Intl.DateTimeFormat('en', { day: '2-digit' }).format(value);
+        let ye = new Intl.DateTimeFormat('en', { year: 'numeric' }).format(startDate);
+        let mo = new Intl.DateTimeFormat('en', { month: '2-digit' }).format(startDate);
+        let da = new Intl.DateTimeFormat('en', { day: '2-digit' }).format(startDate);
         console.log(`${da}-${mo}-${ye}`);
 
         // en-GB locale format dd/mm/yyyy
         let o = new Intl.DateTimeFormat("en-GB" , {
             dateStyle: "short"
         });
-        console.log(o.format(value))
+        console.log(o.format(startDate))
 
     }
     return (
         <div>
-            <Calendar id = "calendar"
-                onChange={onChange}
-                value={value}
-            />
-            <button onClick={getCalendarData}>React Calendar</button>
+            <div>
+                <DatePicker
+                    dateFormat="dd-MM-yyyy"
+                    selected={startDate}
+                    onChange={date => setStartDate(date)} />
+                <button onClick={getCalendarData}>React Calendar</button>
+            </div>
         </div>
     );
 }
